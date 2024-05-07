d4857e6b0f20a9d69100cbb69c2714619d7714e4
Remaydo Timothy Gultom <139677909+gultom017@users.noreply.github.com>
2024-05-07 23:09:15 +0700

    Compare to Main

--- main.go
+++ main.go
@@ -31,7 +31,6 @@ func main() {
 	router.POST(apiVersion+"ProductPrice", ProductPrice)
 	router.POST(apiVersion+"ProductStock", ProductStock)
 	router.POST(apiVersion+"ScanProduct", ScanProduct)
-	router.POST(apiVersion+"MasterProdukEach", ScanProduct)
 
 	PORT := os.Getenv("PORT")
 
